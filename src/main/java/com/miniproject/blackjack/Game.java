343875990a1a53ccd6fbfc6a93f2d5cb04761e35
Ayrat Gimranov <gimranov45@gmail.com>
2022-08-08 12:59:01 -0700

    created Game class

---
+++ src/main/java/com/miniproject/blackjack/Game.java
@@ -0,0 +1,19 @@
+package com.miniproject.blackjack;
+
+public class Game {
+
+  private int money;
+  // TODO create dealer
+  //  private Dealer dealer;
+
+  // TODO create player
+  // private Player player;
+
+
+  public Game(){
+    money = 1000;
+  }
+
+
+
+}
