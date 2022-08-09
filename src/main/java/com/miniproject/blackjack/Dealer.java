e0191b3f940b469fcaa427f11f085f4c54d90c6c
scaffidic <cp.scaffidi@gmail.com>
2022-08-08 19:06:37 -0700

    Added dealer and player logic

--- src/main/java/com/miniproject/blackjack/Dealer.java
+++ src/main/java/com/miniproject/blackjack/Dealer.java
@@ -1,16 +1,28 @@
 package com.miniproject.blackjack;
 
+import java.util.ArrayList;
+
 public class Dealer extends Person {
+
+  Hand hand = new Hand();
 
   public Dealer () {
     setName("Dealer");
   }
 
-  public void printDealerHand() {
-//    System.out.println("Dealer has " + super.getHand().getCard(0));
+  public void printDealerHand() { // need a getter for card
+//    System.out.println("Dealer is showing:" + super.getHand().getCard(0));
   }
 
-  public void dealerPlay() {
-// if value < 17 hit, else stand
+  public void dealerPlay(Deck deck) {
+   while (hand.getScore() <= 16) {
+     System.out.println("Dealer has " + hand.getScore() + " and hits.");
+     hand.addToHand(deck.draw());
+   }
+   if (hand.getScore() > 21) {
+     System.out.println("Dealer busts. You win!");
+   } else {
+     System.out.println("Dealer stands.");
+   }
   }
 }
--- src/main/java/com/miniproject/blackjack/Player.java
+++ src/main/java/com/miniproject/blackjack/Player.java
@@ -1,13 +1,15 @@
 package com.miniproject.blackjack;
 
+import java.util.Scanner;
+
 public class Player extends Person {
 
+  Scanner input = new Scanner(System.in);
+//  private int bank;
+//
+//  private int bet;
 
-  private int bank;
-
-  private int bet;
-
-  private Hand hand;
+  private Hand hand = new Hand();
 
 
   public Player() {
@@ -15,5 +17,31 @@ public class Player extends Person {
     hand = new Hand();
   }
 
+  public void hitOrStand(Deck deck, Deck discard) {
+    int decision = 0;
+    boolean inputNum = true;
 
-}
+    while (inputNum) {
+      try {
+        System.out.println("Press (1) to Hit or (2) to Stand.");
+        decision = input.nextInt();
+        inputNum = false;
+
+      } catch (IllegalArgumentException e) {
+        System.out.println("Invalid entry");
+        input.next();
+      }
+      if (decision == 1) {
+        hand.addToHand(deck.draw());
+        if (this.getHand().getScore() > 21) {
+          return;
+        }
+        else {
+          this.hitOrStand(deck, discard);
+        }
+        } else {
+        System.out.println("You have chosen to Stand.");
+        }
+      }
+    }
+  }
