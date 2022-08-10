602db4529b8defb370541cbf8188d82fec328569
dsk0057 <dkafle6141@gmail.com>
2022-08-10 12:48:07 -0400

    Starting Javadoc and PlayerTest.

--- src/main/java/com/miniproject/blackjack/Main.java
+++ src/main/java/com/miniproject/blackjack/Main.java
@@ -1,6 +1,17 @@
 package com.miniproject.blackjack;
 
+/**
+ * Demonstrates the simple console-mode Blackjack gameplay.
+ */
+
 public class Main {
+
+  /**
+   * Creates an instance of {@link Game}, in order to keep playing until the
+   * false boolean is returned.
+   *
+   * @param args Command-line arguments
+   */
 
   public static final String ANSI_RESET = "\u001B[0m";
   public static final String ANSI_GREEN = "\u001B[32m";
@@ -16,6 +27,5 @@ public class Main {
     while(keepPlaying){
     keepPlaying = game.startRound();
     }
-
   }
 }
---
+++ src/test/java/com/miniproject/blackjack/PlayerTest.java
@@ -0,0 +1,23 @@
+package com.miniproject.blackjack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.InputMismatchException;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+  Player player = new Player();
+  Deck deck = new Deck();
+  Deck discard = new Deck();
+
+  @Test
+  public void testException() {
+    Exception e = assertThrows(InputMismatchException.class, () -> player.hitOrStand(deck, discard));
+        assertEquals("An exception was thrown", e.getMessage());
+  }
+
+  @Test
+  void hitOrStand() {
+  }
+}
